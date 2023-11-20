68b91f615e04516efbd563115ffd509fe0ef0dc7
Aldin Kapetanovic <aldinkapetanovic@gmail.com>
2023-11-21 00:17:27 +0100

    apps

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# povops-do-iaac
+1. **Repository Structure:**
+   - Backend Repo: `main` and `stage` branches.
+   - Frontend Repo: `main` and `stage` branches.
+
+2. **CI/CD Workflow:**
+   - Backend: Separate CI/CD workflows for `main` and `stage` branches.
+   - Frontend: Separate CI/CD workflows for `main` and `stage` branches.
+
+3. **Naming and Tagging Conventions for Docker Images:**
+   - Backend Images:
+     - Main Branch: `your-registry/your-backend-image:main`
+     - Stage Branch: `your-registry/your-backend-image:stage`
+   - Frontend Images:
+     - Main Branch: `your-registry/your-frontend-image:main`
+     - Stage Branch: `your-registry/your-frontend-image:stage`
+
+4. **CI/CD Configuration (GitHub Actions Example):**
+   - Use GitHub Actions for CI/CD.
+   - Single workflow for different branches.
+   - Set up Node.js version using `actions/setup-node`.
+   - Use secrets for sensitive information (e.g., Docker registry credentials).
+
+5. **Testing in CI/CD:**
+   - Include testing steps (unit tests, integration tests) in your workflows before building and pushing Docker images.
+
+
+
+
+tf init
+tf workspace new cr
+
+tf apply
+
+<!-- git checkout apps -->
+
+
+rerun build-push jobs
+
+
+tf workspace new apps
+
+tf apply
+
---
+++ main.tf
@@ -0,0 +1,133 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_app" "povops-app" {
+  spec {
+    name   = "app"
+    region = "fra1"
+
+    service {
+      name = "app"
+      image {
+        registry_type = "DOCR"
+        registry      = ""
+        repository    = "app"
+        tag           = "main"
+        deploy_on_push {
+          enabled = true
+        }
+      }
+
+      http_port = 3000
+
+      alert {
+        rule     = "CPU_UTILIZATION"
+        value    = 50
+        operator = "GREATER_THAN"
+        window   = "FIVE_MINUTES"
+        disabled = false
+      }
+    }
+  }
+}
+
+resource "digitalocean_app" "povops-app-stage" {
+  spec {
+    name   = "app-stage"
+    region = "fra1"
+
+    service {
+      name = "app-stage"
+      image {
+        registry_type = "DOCR"
+        registry      = ""
+        repository    = "app"
+        tag           = "stage"
+        deploy_on_push {
+          enabled = true
+        }
+      }
+
+      http_port = 3000
+
+      alert {
+        rule     = "CPU_UTILIZATION"
+        value    = 50
+        operator = "GREATER_THAN"
+        window   = "FIVE_MINUTES"
+        disabled = false
+      }
+    }
+  }
+}
+
+resource "digitalocean_app" "povops-srv" {
+  spec {
+    name   = "srv"
+    region = "fra1"
+
+    service {
+      name = "srv"
+      image {
+        registry_type = "DOCR"
+        registry      = ""
+        repository    = "srv"
+        tag           = "main"
+        deploy_on_push {
+          enabled = true
+        }
+      }
+
+      http_port = 3000
+
+      alert {
+        rule     = "CPU_UTILIZATION"
+        value    = 50
+        operator = "GREATER_THAN"
+        window   = "FIVE_MINUTES"
+        disabled = false
+      }
+    }
+  }
+}
+
+resource "digitalocean_app" "povops-srv-stage" {
+  spec {
+    name   = "srv-stage"
+    region = "fra1"
+
+    service {
+      name = "srv-stage"
+      image {
+        registry_type = "DOCR"
+        registry      = ""
+        repository    = "srv"
+        tag           = "stage"
+        deploy_on_push {
+          enabled = true
+        }
+      }
+
+      http_port = 3000
+
+      alert {
+        rule     = "CPU_UTILIZATION"
+        value    = 50
+        operator = "GREATER_THAN"
+        window   = "FIVE_MINUTES"
+        disabled = false
+      }
+    }
+  }
+}
