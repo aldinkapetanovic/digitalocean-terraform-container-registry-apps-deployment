a9b199d00f387aae09cc1bfe7a7e4081a6452207
Aldin Kapetanovic <aldinkapetanovic@gmail.com>
2023-11-21 21:41:15 +0100

    tidy

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 tf init
+
 tf workspace new cr
 
 tf apply
--- output.tf
+++ output.tf
@@ -2,16 +2,13 @@ output "povops-app_live_url" {
   value = digitalocean_app.povops-app.live_url
 }
 
-
 output "povops-app-stage_live_url" {
   value = digitalocean_app.povops-app-stage.live_url
 }
 
-
 output "povops-srv_live_url" {
   value = digitalocean_app.povops-srv.live_url
 }
-
 
 output "povops-srv-stage_live_url" {
   value = digitalocean_app.povops-srv-stage.live_url
